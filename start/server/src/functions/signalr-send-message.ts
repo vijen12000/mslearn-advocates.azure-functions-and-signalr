c809e5cd376e98560bfdba8f6d42d45967ec99ce
Vijender Kumar <vijen12000@gmail.com>
2025-03-29 19:24:22 +0530

    Add SignalR functions

---
+++ start/server/src/functions/signalr-send-message.ts
@@ -0,0 +1,36 @@
+import { app, CosmosDBv4FunctionOptions, InvocationContext, output } from "@azure/functions";
+
+const goingOutToSignalR = output.generic({
+    type: 'signalR',
+    name: 'signalR',
+    hubName: 'default',
+    connectionStringSetting: 'SIGNALR_CONNECTION_STRING'
+})
+
+export async function dataToMessage(documents: unknown[], context: InvocationContext) : Promise<void> {
+    try {
+        context.log(`Documents: ${JSON.stringify(documents)}`)
+        documents.map(stock=>{
+            // @ts-ignore
+            context.log(`Get price ${stock.symbol} ${stock.price}`)
+            context.extraOutputs.set(goingOutToSignalR,{
+                'target':'updated',
+                'arguments': [stock]
+            })
+        })
+    } catch (error) {
+        context.log(`Error: ${error}`)
+    } 
+}
+
+const options: CosmosDBv4FunctionOptions ={
+    connection: 'COSMOSDB_CONNECTION_STRING',
+    databaseName: 'stocksdb',
+    containerName: 'stocks',
+    createLeaseContainerIfNotExists: true,
+    feedPollDelay: 500,
+    handler: dataToMessage,
+    extraOutputs: [goingOutToSignalR]
+}
+
+app.cosmosDB('signalr-send-message', options);
