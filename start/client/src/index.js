173d841e48dd5eb89a72d7f7b14030fe795e3e11
Vijender Kumar <vijen12000@gmail.com>
2025-03-29 20:06:34 +0530

    Add BACKEND_URL environment variable

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -42,3 +42,32 @@ const app = new Vue({
         this.startPoll();
     }
 });
+
+const connect = () => {
+
+    const signalR_URL = `${getApiUrl()}/api`;
+    console.log(`Connecting to SignalR...${signalR_URL}`)
+
+    const connection = new signalR.HubConnectionBuilder()
+                            .withUrl(signalR_URL)
+                            .configureLogging(signalR.LogLevel.Information)
+                            .build();
+
+    connection.onclose(()  => {
+        console.log('SignalR connection disconnected');
+        setTimeout(() => connect(), 2000);
+    });
+
+    connection.on('updated', updatedStock => {
+        console.log('Stock updated message received', updatedStock);
+        const index = app.stocks.findIndex(s => s.id === updatedStock.id);
+        console.log(`${updatedStock.symbol} Old price: ${app.stocks[index].price}, New price: ${updatedStock.price}`);
+        app.stocks.splice(index, 1, updatedStock);
+    });
+
+    connection.start().then(() => {
+        console.log("SignalR connection established");
+    });
+};
+
+connect();
